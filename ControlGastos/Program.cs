fed0fe86986db03469fafcdba08960addce0fbf3
Alan-Agetro-Dev <alanagetrodev@protonmail.com>
2026-04-27 22:24:14 -0600

    Lectura de datos de el usuario y uso de variables

--- ControlGastos/Program.cs
+++ ControlGastos/Program.cs
@@ -4,7 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bienvenido al sistema de control de gastos");
+        Console.WriteLine("CONTROL DE GASTOS");
+
+        // Pedimos un gasto al usuario
+        Console.Write("Ingresa un gasto: ");
+
+        // Leemos lo que el usuario escribe
+        string entrada = Console.ReadLine();
+
+        // Convertimos el texto a número
+        double gasto = double.Parse(entrada);
+
+        // Mostramos el valor ingresado
+        Console.WriteLine($"Gasto ingresado: ${gasto}");
+
         Console.ReadKey();
     }
 }
