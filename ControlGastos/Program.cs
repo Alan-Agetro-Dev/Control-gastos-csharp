8edc595607c9e3936660ef37d3364d458e366cea
Alan-Agetro-Dev <alanagetrodev@protonmail.com>
2026-04-27 22:06:56 -0600

    Add project files.

---
+++ ControlGastos/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido al sistema de control de gastos");
+        Console.ReadKey();
+    }
+}
